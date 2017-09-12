6bf81495b80709a85c9cae0d0847df4897c156db
Rabiu Ademoh <rabiu@cellulant.com.ng>
2017-09-12 14:45:30 +0100

    Completed the interface theme and other issues

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,9 +1,7 @@
 package net.kzn.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller 
@@ -11,31 +9,30 @@ public class PageController {
 
 	@RequestMapping(value = {"/","/home","/index"})
 	public ModelAndView index() {
-		
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greetings","Welcome to Spring Web  MVC");
+		mv.addObject("title","Home");
+		mv.addObject("userClickHome",true);
 		return mv;
 	}
 	
-	/*@RequestMapping(value = {"/test"})
-	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting){
-		if(greeting == null) {
-			greeting = "Hello There";
-		}
+	@RequestMapping(value = "/about")
+	public ModelAndView about() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greetings",greeting);
+		mv.addObject("title","About Us");
+		mv.addObject("userClickAbout",true);
 		return mv;
-	}*/
+	}
+	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title","Contact Us");
+		mv.addObject("userClickContact",true);
+		return mv;
+	}
+	
+	
 	
 
-	@RequestMapping(value = "/test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting")String greeting){
-		if(greeting == null) {
-			greeting = "Hello There";
-		}
-		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greetings",greeting);
-		return mv;
-	}
 	
 }
