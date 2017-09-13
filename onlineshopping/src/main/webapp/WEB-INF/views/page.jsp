2d9e5c251057d703fb32a92293a26772b6508ae4
Rabiu Ademoh <rabiu@cellulant.com.ng>
2017-09-13 17:51:27 +0100

    Successfully updated the UI and linked with backend project !

--- onlineshopping/src/main/webapp/WEB-INF/views/page.jsp
+++ onlineshopping/src/main/webapp/WEB-INF/views/page.jsp
@@ -68,6 +68,11 @@
  <%@include file="contact.jsp" %>
 </c:if>
 
+<!-- Load when user clicks products -->
+<c:if test="${userClickAllProducts == true or userClickCategoryProducts == true}">
+ <%@include file="listProducts.jsp" %>
+</c:if>
+
  </div>
  
  
---
+++ shoppingbackend/src/main/java/net/kzn/shoppingbackend/daoimpl/CategoryDAOImpl.java
@@ -0,0 +1,69 @@
+package net.kzn.shoppingbackend.daoimpl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import net.kzn.shoppingbackend.dao.CategoryDAO;
+import net.kzn.shoppingbackend.dto.Category;
+
+@Repository("categoryDAO")
+public class CategoryDAOImpl implements CategoryDAO {
+	
+	private static List<Category> categories = new ArrayList<>();
+	
+	static {
+		
+		Category category = new Category();
+		
+		///adding 1
+		category.setId(1);
+		category.setName("Television");
+		category.setDescription("This is description for television");
+		category.setImageURL("CAT_1.png");
+		
+		categories.add(category);
+		
+		///adding 2
+		category = new Category();
+		category.setId(2);
+		category.setName("Mobile");
+		category.setDescription("This is description for mobile");
+		category.setImageURL("CAT_2.png");
+		
+		categories.add(category);
+				
+		///adding 3
+		category = new Category();
+		category.setId(3);
+		category.setName("Laptop");
+		category.setDescription("This is description for Laptop");
+		category.setImageURL("CAT_3.png");
+				
+		categories.add(category);		
+		
+		
+	}
+
+	@Override
+	public List<Category> list() {
+		// TODO Auto-generated method stub
+		return categories;
+	}
+
+	@Override
+	public Category get(int id) {
+		
+		///enchanced for loop
+		for(Category category : categories) {
+			
+			if(category.getId() == id) {
+				
+				return category;
+			}
+		}
+		return null;	
+	}
+
+}
